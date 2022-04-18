58778c676f45f787fcc68c7a0079374e247da078
Viktor <viktorbezai@gmail.com>
2022-04-18 18:30:01 +0300

    Chapter 8 - Done

--- posts/permissions.py
+++ posts/permissions.py
@@ -8,3 +8,12 @@ class IsAuthorOrReadOnly(permissions.BasePermission):
             return True
         # Write permissions are only allowed to the author of a post
         return obj.author == request.user
+
+
+class IsUserOrReadOnly(permissions.BasePermission):
+    def has_object_permission(self, request, view, obj):
+        # Read-only permissions are allowed for any request
+        if request.method in permissions.SAFE_METHODS:
+            return True
+        # Write permissions are only allowed to the author of a post
+        return obj.username == request.user.username
--- posts/views.py
+++ posts/views.py
@@ -1,15 +1,18 @@
-from rest_framework import generics
+from django.contrib.auth import get_user_model
+from rest_framework import viewsets
 from .models import Post
-from .permissions import IsAuthorOrReadOnly
-from .serializers import PostSerializer
+from .permissions import IsAuthorOrReadOnly, IsUserOrReadOnly
+from .serializers import PostSerializer, UserSerializer
 
 
-class PostList(generics.ListCreateAPIView):
+class PostViewSet(viewsets.ModelViewSet):
+    permission_classes = (IsAuthorOrReadOnly, )
     queryset = Post.objects.all()
     serializer_class = PostSerializer
 
 
-class PostDetail(generics.RetrieveUpdateDestroyAPIView):
-    permission_classes = (IsAuthorOrReadOnly,)
-    queryset = Post.objects.all()
-    serializer_class = PostSerializer
+class UserViewSet(viewsets.ModelViewSet):
+    permission_classes = (IsUserOrReadOnly,)
+    queryset = get_user_model().objects.all()
+    serializer_class = UserSerializer
+
